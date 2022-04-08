0ae58fe500aac6f25d612582844b67be35f6ee25
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-04-08 19:03:02 -0300

    font Open Sans added

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,9 @@ import './css/index.css';
 import App from './App';
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Alumni+Sans+Inline+One&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
 </style>
+
 
 ReactDOM.render(
   <React.StrictMode>
