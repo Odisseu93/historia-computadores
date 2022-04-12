0e952f8002a0055375c4dd6516638bdf1c092efa
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-04-11 21:06:13 -0300

    component added

---
+++ src/components/articles/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface Props{
+  textP: string;
+  url_image: string;
+  alt_image: string;
+  ref_text: string;
+  ref_a_url: string;
+  ref_a_text: string;
+}
+
+
+const Article = ({ textP, url_image,  alt_image, ref_text, ref_a_url, ref_a_text }: Props)=> {
+  return(
+  <article className='art'>
+    <p>{textP}</p>
+    <div className='picture_and_text'>
+      <img src={url_image} width="50%" alt={alt_image} className="art_img" />
+      <p>
+        <p className="ref_info">
+          {ref_text}: <a href={ref_a_url} className='link'>{ref_a_text}</a>
+          <br />
+        </p>
+        </p>
+    </div>
+  </article>
+  );
+}
+
+export default Article;
