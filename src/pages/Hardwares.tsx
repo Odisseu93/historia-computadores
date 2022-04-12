eaa912d296f1752437d2a032d46b924880d3c313
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-04-11 21:47:02 -0300

    added content on page

--- src/pages/Hardwares.tsx
+++ src/pages/Hardwares.tsx
@@ -1,12 +1,41 @@
 import React from "react";
 import Header from "../components/header";
+import Article from "../components/articles";
+import References from "../components/references";
 
-function Hardwares() { 
-  return(
-  <div className="container">
-    <Header />
-    <h1>This is Hardware page!</h1>
-  </div>  
-);
+function Hardwares() {
+  return (
+    <div className="container">
+      <Header />
+      <main>
+        <h1>Hardwares</h1>
+        <br />
+        <section>
+          <h2>Armazenamento de dados</h2>
+          <Article
+            textP="Os dispositivos de armazenamento por meio magnético são os mais antigos e mais utilizados atualmente, por permitir uma grande densidade de informação, ou seja, armazenar grande quantidade de dados em um pequeno espaço físico. São mais antigos, porém foram se aperfeiçoando no decorrer do tempo.
+           Os dispositivos de armazenamento magnéticos que possuem mídias removíveis normalmente não possuem capacidade e confiabilidade equivalente aos dispositivos fixos, pois sua mídia é frágil e possui capacidade de armazenamento muito pequena se comparada a outros tipos de dispositivos de armazenamento magnéticos. Como exemplo de dispositivos de armazenamento por meio magnético, podemos citar os Discos Rígidos."
+           url_image="https://br.crucial.com/content/dam/crucial/articles/pc-users/why-is-my-laptop-so-slow-/hard-disk-drive.jpg.transform/large-jpg/img.jpg"
+            alt_image="hard drive"
+            ref_text="O que um disco rígido"
+            ref_a_url="https://www.infoescola.com/informatica/disco-rigido/"
+            ref_a_text="Clique aqui!"
+          />
+        </section>
+        <br/>
+        <article className='art'>
+          <p>Links, Referencias e Informações:</p>
+          <References
+            referece_title="A Evolução dos Discos Rígidos"
+            referece_url="https://www.techtudo.com.br/platb/hardware/2011/01/06/evolucao-discos-rigidos-hd/"
+          />
+          <References
+            referece_title="Memórias RAM e ROM"
+            referece_url="https://www.infowester.com/memoria.php"
+          />
+        </article>
+      </main>
+    </div>
+  );
 }
 export default Hardwares;
