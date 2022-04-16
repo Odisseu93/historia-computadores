c9ba3b66b209c0032629f1b5166faea08f68835d
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-04-16 20:48:51 -0300

    Header update

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,16 +2,35 @@
 import React from "react";
 
 //criando uma class "Header" extendedo react component
+// function Header() {
+//   return ( //return  obrigatório
+//     <div className="container">
+//       <header className="hdr">
+//         <nav className="hdrmenu">
+//           <a href='/'>A Historia dos Computadores</a>
+//           <a href="/#/Hardwares">Hardwares</a>
+//           <a href="/#/Softwares">Softwares</a>
+//           <a href="/#/Mobile">Mobile</a>
+//           <a href="/#/Sobre">Sobre</a>
+//         </nav>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default Header;
+
+//header depois do Deployment
 function Header() {
   return ( //return  obrigatório
     <div className="container">
       <header className="hdr">
         <nav className="hdrmenu">
           <a href='/'>A Historia dos Computadores</a>
-          <a href="/#/Hardwares">Hardwares</a>
-          <a href="/#/Softwares">Softwares</a>
-          <a href="/#/Mobile">Mobile</a>
-          <a href="/#/Sobre">Sobre</a>
+          <a href="https://odisseu93.github.io/historia-computadores/#/Hardwares">Hardwares</a>
+          <a href="https://odisseu93.github.io/historia-computadores/#/softwares">Softwares</a>
+          <a href="https://odisseu93.github.io/historia-computadores/#/mobile">Mobile</a>
+          <a href="https://odisseu93.github.io/historia-computadores/#/sobre">Sobre</a>
         </nav>
       </header>
     </div>
